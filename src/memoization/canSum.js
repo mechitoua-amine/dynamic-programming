221280d48a810c7face159b301ed8840ff5cbf92
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-03 12:14:59 +0100

    adding canSum() problem inside memoization

---
+++ src/memoization/canSum.js
@@ -0,0 +1,13 @@
+/*
+  Write a function `canSum(targetSum, numbers)` that takes in a targetSum
+  and an array of numbers as arguments.
+  The function should return a boolean indicating whether or not it is
+  possible to generate the targetSum using numbers from the array.
+
+  you may use an element of the array as many times as needed.
+  you may assume that all input numbers are non-negative.
+*/
+
+const canSum(targetSum, numbers) => {
+  // implementation here
+}
