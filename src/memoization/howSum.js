86a71d3149f8469527597c483ee0376d0b479212
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-04 10:28:50 +0100

    solving howSum problem with brute force

---
+++ src/memoization/howSum.js
@@ -0,0 +1,30 @@
+/*
+  write a function `howSum(targetSum, numbers)` that takes in a targetSum
+  and ann array of numbers as arguments.
+  The function should return an array containing any combination of
+  elements that add up to exactly the targetSum. if there is no
+  combinaiton that adds up to the targetSum, then return null.
+
+  if there are multiple combinations possible, you may return any single one
+*/
+
+/*
+  Brute force solution
+  m = target sum
+  n = array's length
+  O(n^m * m) time
+  O(m) space
+ */
+const howSum = (targetSum, numbers) => {
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const result = howSum(remainder, numbers);
+    if (result !== null) {
+      return [...result, num];
+    }
+  }
+  return null;
+}
