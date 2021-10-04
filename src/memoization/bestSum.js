938e4bee94a483ebbdf08a1bf769f37a2227ad0e
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-04 11:16:27 +0100

    implementing the brute force solution for bestSum

--- src/memoization/bestSum.js
+++ src/memoization/bestSum.js
@@ -1,3 +1,4 @@
+/* esversion: 6 */
 /*
   Write a function `bestSum(targetSum, numbers)` that takes in a targetSum
   and an array of numbers as arguments
@@ -8,6 +9,33 @@
   shortest combinations.
  */
 
-const bestsum = (targetSum, numbers) => {
-  // implementation here
-}
+/*
+  Brute force solution
+  m = targetSum
+  n = array's length
+  time: O(n^m * m)
+  space: O(m^2)
+ */
+const bestSum = (targetSum, numbers) => {
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  let shortestCombination = null;
+
+  for (let num of numbers) {
+    const remiander = targetSum - num;
+    const result = bestSum(remiander, numbers);
+    if (result !== null) {
+      const combination = [...result, num];
+      if (shortestCombination === null || combination.length < shortestCombination.length) {
+        shortestCombination = combination;
+      }
+    }
+  }
+  return shortestCombination;
+};
+
+console.log(bestSum(7, [5, 3, 4, 7]));
+console.log(bestSum(8, [2, 3, 5]));
+console.log(bestSum(8, [1, 4, 5]));
+// console.log(bestSum(100, [1, 2, 3, 25]));
