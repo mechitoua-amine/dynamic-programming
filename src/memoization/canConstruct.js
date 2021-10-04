cadd2c6057356b9fc97b6b4f5a4d8e687155200b
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-04 11:47:48 +0100

    adding canConstruct problem

---
+++ src/memoization/canConstruct.js
@@ -0,0 +1,15 @@
+/*
+  Write a funciton `canConstruct(target, wordBank)` that accepts a target
+  string and an array of strings
+  The function should return a boolean indication whether or not the target
+  can be constructed by concatenating elements of the wordbank array
+
+  You may reuse elements of wordBank as many times as needed
+ */
+
+const canConstruct = (target, wordBank) => {
+  // implementation here
+};
+
+console.log("abcdef", [ab, abc, cd, def, abcd]);
+console.log("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]);
