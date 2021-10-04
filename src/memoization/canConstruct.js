6ea8e3ce46891e9c1d759eac455a5ff809a481a9
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-04 12:10:10 +0100

    implementing the brute force solution for canConstruct

--- src/memoization/canConstruct.js
+++ src/memoization/canConstruct.js
@@ -7,9 +7,24 @@
   You may reuse elements of wordBank as many times as needed
  */
 
+/*
+ Brute force solution
+  m = target
+  n = wordBank.length
+  Time: O(n^m * m)
+  Space: O(m * n)
+ */
 const canConstruct = (target, wordBank) => {
-  // implementation here
+  if (target === "") return true;
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      if (canConstruct(suffix, wordBank) === true) return true;
+    }
+  }
+
+  return false;
 };
 
-console.log("abcdef", [ab, abc, cd, def, abcd]);
+console.log("abcdef", ["ab", "abc", "cd", "def", "abcd"]);
 console.log("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]);
