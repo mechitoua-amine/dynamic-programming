06a6cb55f869148f2cef8d2ceac9ca758cc0d445
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-05 09:53:53 +0100

    setting up countConstruct problem

---
+++ src/memoization/countConstruct.js
@@ -0,0 +1,16 @@
+/*
+  Write a function `countConstruct(target, wordBank)` that accepts a target
+  string and an array of strings
+  The function should return the number of ways that the target can be
+  constructed by concatenating elements of the wordBank array
+
+  You may reuse elemnts of wordBank as many times as needed
+ */
+
+const countConstruct = (target, wordBank) => {
+  // implementation here
+}
+
+console.log(countConstruct("purple", ["puep", "p", "ur", "le", "purpl"]));
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]));
