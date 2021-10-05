834333645375a1eb09e392e7ec643b6f67393ebf
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-05 10:03:41 +0100

    implementing the memoized solution for countConstruct

--- src/memoization/countConstruct.js
+++ src/memoization/countConstruct.js
@@ -7,18 +7,38 @@
   You may reuse elemnts of wordBank as many times as needed
  */
 
-
 // brute force solution
-const countConstruct = (target, wordBank) => {
+// Time: O(n^m * m)
+// Space: O(m^2)
+// const countConstruct = (target, wordBank) => {
+//   if (target === '') return 1;
+
+//   let totalcount = 0;
+//   for (let word of wordBank) {
+//     if (target.indexOf(word) === 0) {
+//       const numOfWays = countConstruct(target.slice(word.length), wordBank)
+//       totalcount += numOfWays;
+//     }
+//   }
+//   return totalcount;
+// }
+
+
+// memoized solution
+// Time: O(n * m^2)
+// Space: O(m^2)
+const countConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
   if (target === '') return 1;
 
   let totalcount = 0;
   for (let word of wordBank) {
     if (target.indexOf(word) === 0) {
-      const numOfWays = countConstruct(target.slice(word.length), wordBank)
+      const numOfWays = countConstruct(target.slice(word.length), wordBank, memo)
       totalcount += numOfWays;
     }
   }
+  memo[target] = totalcount;
   return totalcount;
 }
 
