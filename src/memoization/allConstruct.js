44fc4c1d26af262c6953459d59410e6381be28c6
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-05 13:29:42 +0100

    implementing the memoized solution for allConstruct

--- src/memoization/allConstruct.js
+++ src/memoization/allConstruct.js
@@ -16,7 +16,28 @@
   Time: O(n^m)
   Space: O(m)
  */
-const allConstruct = (target, wordBank) => {
+// const allConstruct = (target, wordBank) => {
+//   if (target === "") return [
+//     []
+//   ];
+
+//   const result = []
+
+//   for (let word of wordBank) {
+//     if (target.indexOf(word) === 0) {
+//       const suffix = target.slice(word.length);
+//       const suffixWays = allConstruct(suffix, wordBank);
+//       const targetWays = suffixWays.map(way => [word, ...way]);
+//       result.push(...targetWays);
+//     }
+//   }
+
+//   return result;
+// }
+
+// memoized solution
+const allConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) memo[target];
   if (target === "") return [
     []
   ];
@@ -26,14 +47,15 @@ const allConstruct = (target, wordBank) => {
   for (let word of wordBank) {
     if (target.indexOf(word) === 0) {
       const suffix = target.slice(word.length);
-      const suffixWays = allConstruct(suffix, wordBank);
+      const suffixWays = allConstruct(suffix, wordBank, memo);
       const targetWays = suffixWays.map(way => [word, ...way]);
       result.push(...targetWays);
     }
   }
-
+  memo[target] = result;
   return result;
 }
+
 
 console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
 console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]));
