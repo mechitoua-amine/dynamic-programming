50eb956f247d27abc64dc5e6307798f9583d6b73
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-10-05 10:21:40 +0100

    setting up allConstruct problem

---
+++ src/memoization/allConstruct.js
@@ -0,0 +1,18 @@
+/*
+  Write a function 'allConstruct(target, wordBank)' that accepts a target
+  string and an array of strings
+  The function should return a 2D array containing all of the ways that
+  the target can be constructed by concatenatin elements of the wordBank
+  array. Each element of the 2D array should represent one combination that
+  constructs the target
+
+  You may reuse elements of wordBank as many times as needed
+ */
+
+const allConstruct = (target, wordBank) => {
+  // implementation
+}
+
+console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]));
